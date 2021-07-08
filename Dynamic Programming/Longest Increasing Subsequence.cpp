d3692d0b70382fbfddcca48b63aad090403ed35f
Chandramani Kumar <56932293+hackifme1@users.noreply.github.com>
2021-07-08 14:44:10 +0530

    Added Cpp code for Longest Increasing Subsequence

---
+++ Dynamic Programming/Longest Increasing Subsequence.cpp
@@ -0,0 +1,84 @@
+// By Chandramani Kumar
+// Keep moving and must be simple bro!!!!!
+// Espérons le meilleur mais préparez-vous au pire 😎
+
+
+/* Problem Statement :
+
+Find the longest increasing subsequence of a given array of integers, A.
+
+In other words, find a subsequence of array in which the subsequence’s elements are in strictly increasing order, and in which the subsequence is as long as possible. 
+
+This subsequence is not necessarily contiguous, or unique.
+
+In this case, we only care about the length of the longest increasing subsequence.
+
+
+
+Input Format:
+
+The first and the only argument is an integer array A.
+
+Output Format:
+
+Return an integer representing the length of the longest increasing subsequence.
+
+
+
+Constraints:
+
+1 <= length(A) <= 2500
+1 <= A[i] <= 2000
+
+
+
+Example :
+
+Input 1:
+    A = [1, 2, 1, 5]
+
+Output 1:
+    3
+    
+Explanation 1:
+    The sequence : [1, 2, 5]
+
+Input 2:
+    A = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+    
+Output 2:
+    6
+
+Explanation 2:
+    The sequence : [0, 2, 6, 9, 13, 15] or [0, 4, 6, 9, 11, 15] or [0, 4, 6, 9, 13, 15]
+
+*/
+
+
+Cpp code :
+
+int Solution::lis(const vector<int> &A) {
+        int res[2500] = {0};
+        int maxm = INT_MIN;
+
+        for(int i = 0; i < A.size(); i++)
+        {
+            int prev_maxm = 0;
+
+            for(int j = 0; j < i; j++)
+            {
+                if(A[j] < A[i])
+                {
+                    if(res[j] > prev_maxm)
+                    {
+                        prev_maxm = res[j];
+                    }
+                }
+            }
+
+            res[i] = prev_maxm + 1;
+            maxm = max(maxm,res[i]);
+        }
+        
+        return maxm;
+}
