c6629873d9be8cc338c63ada587d2909f55e34f8
Chandramani Kumar <56932293+hackifme1@users.noreply.github.com>
2021-07-27 21:04:20 +0530

    Added BFS traversal approach

--- Graph Data Structure & Algorithms/Cycle in Undirected Graph.cpp
+++ Graph Data Structure & Algorithms/Cycle in Undirected Graph.cpp
@@ -78,6 +78,8 @@ Explanation 2:
 
 Cpp code :
 
+// using DFS traversal
+
 	bool dfs(vector<int> adj[], vector<bool> &vis, int prnt, int node)
 	{
 	    vis[node] = true;
@@ -112,6 +114,58 @@ int Solution::solve(int A, vector<vector<int> > &B) {
 	   	    if(!vis[i])
 	   	    {
     	   	    if(dfs(adj, vis, -1, i))
+    	   	    return true;
+	   	    }
+	   	}
+	   	
+	return false;
+}
+
+
+// or using BFS traversal
+
+	bool bfs(vector<int> adj[], vector<bool> &vis, int prnt, int node)
+	{
+	   queue<pair<int,int> > que;
+
+	   que.push(make_pair(node, prnt));
+	   
+	   while(!que.empty())
+	   {
+		   pair<int,int> frnt = que.front();
+		   vis[frnt.first] = true;
+		   que.pop();
+
+		   for(auto ver : adj[frnt.first])
+		   {
+			   if(!vis[ver])
+			   {
+				  que.push(make_pair(ver,frnt.first));
+			   }
+			   else if(frnt.second != ver)
+			   return true;
+		   }
+	   }
+
+	   return false;
+	}
+
+int Solution::solve(int A, vector<vector<int> > &B) {
+    vector<int> adj[A+1];
+    
+    for(int i=0;i<B.size();i++)
+    {
+        adj[B[i][0]].push_back(B[i][1]);
+        adj[B[i][1]].push_back(B[i][0]);
+    } 
+
+    vector<bool> vis(A+1,false);
+    
+	   	for(int i=1;i<=A;i++)
+	   	{
+	   	    if(!vis[i])
+	   	    {
+    	   	    if(bfs(adj, vis, -1, i))
     	   	    return true;
 	   	    }
 	   	}
